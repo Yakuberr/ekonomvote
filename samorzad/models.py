5cfe90ece40dcc506784ac22921ab6bde8c3c5c7
Yakuberr <YakubKr@proton.me>
2025-04-02 10:24:05 +0200

    feat: Usprawniam relację pomiędzy głosem, kandydatem i głosowaniem

--- samorzad/models.py
+++ samorzad/models.py
@@ -56,30 +56,36 @@ class ElectoralProgram(models.Model):
     def __str__(self):
         return f'ElectoralProgram(candidate={self.candidate.pk}, voting={self.voting.pk})'
 
+class CandidateRegistration(models.Model):
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE, related_name='registrations')
+    voting = models.ForeignKey(Voting, on_delete=models.CASCADE, related_name='candidate_registrations')
+    is_eligible = models.BooleanField(default=False)  # Przeniesione z modelu Candidate
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['candidate', 'voting'], name='unique_candidate_per_voting')
+        ]
+
+    def __str__(self):
+        return f'CandidateRegistration(candidate={self.candidate.pk}, voting={self.voting.pk})'
+
 class Vote(models.Model):
-    voting = models.ForeignKey(Voting, on_delete=models.CASCADE, related_name='votes', null=False)
-    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE, related_name='votes', null=False)
+    candidate_registration = models.ForeignKey(CandidateRegistration, on_delete=models.CASCADE, related_name='votes')
     azure_user_id = models.CharField(null=False, max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['voting', 'azure_user_id'], name='unique_user_vote_per_voting')
+            models.UniqueConstraint(fields=['candidate_registration__voting', 'azure_user_id'],
+                                   name='unique_user_vote_per_voting')
         ]
 
-    def __str__(self):
-        return f'Vote(azure_user={self.azure_user_id}, candidate={self.candidate.pk}, voting={self.voting.pk})'
-
     def clean(self):
-        if timezone.now() < self.voting.planned_start:
+        voting = self.candidate_registration.voting
+        if timezone.now() < voting.planned_start:
             raise ValidationError("Nie można głosować przed rozpoczęciem głosowania")
-        if timezone.now() > self.voting.planned_end:
+        if timezone.now() > voting.planned_end:
             raise ValidationError("Nie można głosować po zakończeniu głosowania")
-        if not self.candidate.is_eligible:
+        if not self.candidate_registration.is_eligible:
             raise ValidationError("Nie można oddać głosu na kandydata, który nie został dopuszczony do wyborów.")
-
-    def save(self, *args, **kwargs):
-        if self.pk:
-            raise ValidationError("Edytowanie modelu Vote jest zabronione!")
-        self.full_clean()
-        super().save(*args, **kwargs)
