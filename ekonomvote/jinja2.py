ca11693f6731818d5ed48e79ee4f3881e9dde52f
Yakuberr <YakubKr@proton.me>
2025-04-05 20:13:05 +0200

    feat: Dodaję settings.MEDIA_URL do globalnych zmiennych jinjy

--- ekonomvote/jinja2.py
+++ ekonomvote/jinja2.py
@@ -2,6 +2,7 @@ from django.templatetags.static import static
 from django.urls import reverse
 from django.contrib import messages
 from django.utils import timezone
+from django.conf import settings
 from jinja2 import Environment
 
 
@@ -13,6 +14,7 @@ def environment(**options):
             "url": reverse,
             'get_messages':messages.get_messages,
             'now':timezone.now,
+            'media_url':settings.MEDIA_URL
         }
     )
     return env
