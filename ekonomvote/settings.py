e270ebff585b60d8d39ebddba22748106d53d097
Yakuberr <YakubKr@proton.me>
2025-05-15 19:10:31 +0200

    feat: zmieniam bazę danych z mysql na postgres

--- ekonomvote/settings.py
+++ ekonomvote/settings.py
@@ -88,21 +88,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ekonomvote.wsgi.application'
 
 
-# Na potrzeby ekonomika bez dockera
-# 1. pip install mysqlclient
-# 2. python manage.py migrate
-# 3. python manage.py loaddata datadump.json
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
     }
 }
 
