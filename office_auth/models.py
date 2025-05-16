c2f3dc29377d9e49615ff0670f6d5e119d46bbc7
Yakuberr <YakubKr@proton.me>
2025-05-16 19:02:37 +0200

    feat: Dodaje ważny komentarz

--- office_auth/models.py
+++ office_auth/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class AzureUser(AbstractUser):
-    # null i blank w polu microsoft_user_id są dodane w celu zgodności z procesem tworzenia superusera w django
-    # Podczas tworzenia rzeczywistych obiektów opartych na id zwracanym przez api Azure należy zawsze te id umieścić w polu microsoft_user_id
+    # null i blank w polu microsoft_user są dodane w celu zgodności z procesem tworzenia superusera w django
+    # Podczas tworzenia rzeczywistych obiektów opartych na id zwracanym przez api Azure należy zawsze te id umieścić w polu microsoft_user
     microsoft_user_id = models.CharField(max_length=256, unique=True, null=True, blank=True)
 
     def __str__(self):
