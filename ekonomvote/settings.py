f45d8cacd3fc61b579c066c9fc8d53450966ea91
Yakuberr <YakubKr@proton.me>
2025-05-21 21:53:34 +0200

    refactor: Usuwam niepotrzebne odwołanie do nieistniejącego middleware

--- ekonomvote/settings.py
+++ ekonomvote/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'office_auth.middleware.AzureADAdminLoginMiddleware'
 ]
 
 ROOT_URLCONF = 'ekonomvote.urls'
