48bc5741861ce120225b4212d340f1fa2deb3a66
Yakuberr <YakubKr@proton.me>
2025-06-19 22:01:25 +0200

    fix: Naprawiam rejestrację modelu AzureUser w django admin

--- office_auth/admin.py
+++ office_auth/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
 from .models import AzureUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
 
-class AzureUserAdmin(admin.ModelAdmin):
+class AzureUserAdmin(UserAdmin):
     exclude = ['microsoft_user_id']
 
 admin.site.register(AzureUser, AzureUserAdmin)
