c99d58bc4d971de4f179150010e8efd0666a3cb3
Yakuberr <YakubKr@proton.me>
2025-06-19 22:02:24 +0200

    feat: Usuwam rejestrację widoku office_auth:home z adresów office_auth

--- office_auth/urls.py
+++ office_auth/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('', views.microsoft_login, name='microsoft_login'),
     path('microsoft-callback/', views.microsoft_callback, name='microsoft_callback'),
     path('logout/', views.logout_view, name='logout'),
-    path('home/', views.home_view, name='home')
 ]
