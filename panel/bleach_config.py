ceaabb768916899f906e7f6bbd1cba2c79b87779
Yakuberr <YakubKr@proton.me>
2025-07-28 21:10:53 +0200

    Poprawki UI

--- panel/bleach_config.py
+++ panel/bleach_config.py
@@ -5,16 +5,34 @@ ALLOWED_TAGS = [
     'a', 'b', 'strong', 'i', 'em', 'u', 'strike', 'blockquote',
     'ul', 'ol', 'li',
     'code', 'pre',
-    'p', 'br', 'hr', 'span', 'div'
+    'p', 'br', 'hr', 'span', 'div',
+    'table', 'thead', 'tbody', 'tfoot', 'tr', 'th', 'td',  # dodane tagi tabel
 ]
 
 ALLOWED_ATTRIBUTES = {
     'a': ['href', 'title', 'rel', 'target'],
     'span': ['style'],
     'div': ['style'],
-    'p': ['style'],
+    'p': ['style', 'align'],  # align czasem jest używany
+    'table': ['border', 'cellpadding', 'cellspacing', 'style', 'width'],
+    'th': ['style', 'colspan', 'rowspan', 'scope'],
+    'td': ['style', 'colspan', 'rowspan'],
+    'tr': ['style'],
 }
 
-ALLOWED_CSS_PROPERTIES = ['text-align', 'font-family', 'color', 'background-color', 'font-size', 'line-height']
+ALLOWED_CSS_PROPERTIES = [
+    'text-align', 'font-family', 'color', 'background-color',
+    'font-size', 'line-height', 'border', 'border-collapse',
+    'border-spacing', 'width', 'height', 'padding', 'margin'
+]
 
 css_sanitizer = CSSSanitizer(allowed_css_properties=ALLOWED_CSS_PROPERTIES)
+
+def clean_html(html):
+    return bleach.clean(
+        html,
+        tags=ALLOWED_TAGS,
+        attributes=ALLOWED_ATTRIBUTES,
+        css_sanitizer=css_sanitizer,
+        strip=True
+    )
--- panel/views/utils.py
+++ panel/views/utils.py
@@ -6,8 +6,8 @@ def get_changed_fields(form):
     d = {}
     for field_name in form.changed_data:
         verbose_name = form.instance._meta.get_field(field_name).verbose_name
-        old_value = form.initial.get(field_name)
-        new_value = form.cleaned_data.get(field_name)
+        old_value = str(form.initial.get(field_name))
+        new_value = str(form.cleaned_data.get(field_name))
         d[verbose_name] = {'old':old_value, 'new':new_value}
     return d
 
